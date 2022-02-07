5d4727c673f34753a2e0fc4b5a6e3a250a1e0167
Lada496 <67321065+Lada496@users.noreply.github.com>
2022-02-07 00:15:56 -0800

    modified mongodb connection and listen process

--- server.js
+++ server.js
@@ -1,32 +1,37 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const methodOverride = require('method-override')
-const Article = require('./models/article')
-const articleRoute = require('./routes/articles')
-const app = express()
-const dotenv = require('dotenv')
-dotenv.config()
-
+const express = require("express");
+const mongoose = require("mongoose");
+const methodOverride = require("method-override");
+const Article = require("./models/article");
+const articleRoute = require("./routes/articles");
+const app = express();
+const dotenv = require("dotenv");
+dotenv.config();
 
 // mongoose.connect('mongodb://localhost:27017/myapp', {
 //     useNewUrlParser: true, useUnifiedTopology: true
 //   });
-mongoose.connect(process.env.MONGO_URL)
-.then(() => console.log('Connected to MongoDB'))
-.catch(err => console.log(err))
+// mongoose.connect(process.env.MONGO_URL)
+// .then(() => console.log('Connected to MongoDB'))
+// .catch(err => console.log(err))
 
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride("_method"));
 
-app.set('view engine', 'ejs')
-app.use(express.urlencoded({ extended: false }))
-app.use(methodOverride('_method'))
+app.get("/", async (req, res) => {
+  const articles = await Article.find().sort({ createdAt: "desc" });
+  res.render("articles/index", { articles: articles });
+});
 
-app.get('/', async (req,res) => {
-    const articles = await Article.find().sort({ createdAt: 'desc' })
-    res.render('articles/index', { articles: articles })
-})
+app.use("/articles", articleRoute);
 
-app.use('/articles', articleRoute)
+const PORT = process.env.PORT || 8000;
 
-app.listen(process.env.PORT || 8000, () => {
-  console.log('Backend server is running')
-})
+mongoose.connect(process.env.MONGODB_URL, () => {
+  console.log("db connected");
+  app.listen(PORT);
+});
+
+// app.listen(process.env.PORT || 8000, () => {
+//   console.log("Backend server is running");
+// });
